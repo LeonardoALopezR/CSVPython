9b831a4ac5553f2e633dd35911939394ae43ae54
Leonardo <leo@MacBook-Air-de-Leonardo.local>
2019-03-26 15:14:16 -0600

    Users

--- makeNewCsv.py
+++ makeNewCsv.py
@@ -3,8 +3,10 @@ import csv
 import itertools
 
 lines=(['No. Pedido','Nombre','Mail','Telefono','Telefono de facturacion','Canasta','Club','Dia Pagado','Total','Tortilla Blanca','Tortilla Azul','Queso','Huevo','Pan integral','Panque integral te verde','Pan integral multigrano','Miel','Frijol','Jitomate','Rosca','Cafe','Pan de caja integral','Panque integral platano','Panque brioche','Panque sin gluten platano','Plan de caja blanco','Pan integral matcha','Panque sin gluten','Pan caja canela y pasas','Panque red velvet','Panque sin gluten matcha','Red velvet','Pan de caja blanco multigrano','Lechuga PZ','Acelga KG','Mermelada mango','Salsa Xoconostle','Sal'],)
+userLines=(['Pagina','Nombre','Canasta','Dia','Club','Email','Telefono','Dia Pagado','Pago'],)
 heather=(['Encabezado'],)
 otherLine=[]
+otherUserLine=[]
 
 with open('orders_export.csv', mode='rt') as f, open('sorted.csv', 'w') as final:
     writer = csv.writer(final, delimiter=',')
@@ -17,14 +19,17 @@ with open('orders_export.csv', mode='rt') as f, open('sorted.csv', 'w') as final
 
 f1 = open("sorted.csv")
 f2 = open("customers_export.csv")
+f3 = open("allUsers.csv")
 
 countLine = 0
 countLine2= 0
+countLine3= 0
 orderNumber = 0
 index = 0
 
 csv_f1 = csv.reader(f1)
 csv_f2 = csv.reader(f2)
+csv_f3 = csv.reader(f3)
 
 for row1 in itertools.izip_longest(csv_f1):
     if countLine == 0:
@@ -64,6 +69,15 @@ for row1 in itertools.izip_longest(csv_f1):
         else:
             print "No"
             countLine2 =0
+            countLine3 =0
+            for row3 in itertools.izip_longest(csv_f3):
+                # print 'aaaaaaaaaaa',row3[0][4],row1[0][1]
+                if countLine2==0:
+                    print "Norrl"
+                else:
+                    if row3[0][4]==row1[0][1]:
+                        otherUserLine.append(['p',row3[0][0],row3[0][1],row3[0][2],row3[0][3],row3[0][4]+" ",row3[0][5],row1[0][3][:10],row1[0][11]])
+            countLine3 += 1
             for row2 in itertools.izip_longest(csv_f2):
                 if countLine2==0:
                     print "Norrl"
@@ -85,6 +99,8 @@ for row1 in itertools.izip_longest(csv_f1):
     countLine += 1
     f2.seek(0) 
     next(csv_f2)
+    f3.seek(0) 
+    next(csv_f3)
 print countLine
 with open('entrega.csv', 'wb') as writeFile:
     writer = csv.writer(writeFile,delimiter=',')
@@ -97,5 +113,19 @@ with open('entrega.csv', mode='rt') as f, open('entrega_final.csv', 'w') as fina
     next(reader)
     sorted2 = sorted(reader, key=lambda row: (str(row[6]))) 
     writer.writerows(lines)       
+    for row in sorted2: 
+        writer.writerow(row)
+
+with open('entrega_usuarios.csv', 'wb') as writeFile:
+    writer = csv.writer(writeFile,delimiter=',')
+    writer.writerows(userLines) 
+    writer.writerows(otherUserLine) 
+
+with open('entrega_usuarios.csv', mode='rt') as f, open('entrega_usuarios_final.csv', 'w') as final:
+    writer = csv.writer(final, delimiter=',')
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    sorted2 = sorted(reader, key=lambda row: (str(row[4]))) 
+    writer.writerows(userLines)       
     for row in sorted2: 
         writer.writerow(row)
