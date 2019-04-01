9913bed0277470ee7ead0cfaca30f1eebb5c7635
Leonardo <leo@MacBook-Air-de-Leonardo.local>
2019-03-31 22:40:43 -0600

    Windows

--- makeNewCsv.py
+++ makeNewCsv.py
@@ -1,4 +1,5 @@
-# encoding: utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import csv
 import itertools
 
@@ -8,7 +9,7 @@ heather=(['Encabezado'],)
 otherLine=[]
 otherUserLine=[]
 
-with open('orders_export.csv', mode='rt') as f, open('sorted.csv', 'w') as final:
+with open('orders_export.csv', encoding='utf-8', mode='rt') as f, open('sorted.csv', encoding='utf-8', mode='w', newline='') as final:
     writer = csv.writer(final, delimiter=',')
     reader = csv.reader(f, delimiter=',')
     next(reader)
@@ -17,9 +18,9 @@ with open('orders_export.csv', mode='rt') as f, open('sorted.csv', 'w') as final
     for row in sorted2: 
         writer.writerow(row)
 
-f1 = open("sorted.csv")
-f2 = open("customers_export.csv")
-f3 = open("allUsers.csv")
+f1 = open("sorted.csv", encoding='utf-8')
+f2 = open("customers_export.csv", encoding='utf-8')
+f3 = open("allUsers.csv", encoding='utf-8')
 
 countLine = 0
 countLine2= 0
@@ -31,12 +32,12 @@ csv_f1 = csv.reader(f1)
 csv_f2 = csv.reader(f2)
 csv_f3 = csv.reader(f3)
 
-for row1 in itertools.izip_longest(csv_f1):
+for row1 in itertools.zip_longest(csv_f1):
     if countLine == 0:
         pass
     else:
         entrega = row1[0][17].split()
-        print row1[0][1], orderNumber, len(entrega)
+        # print row1[0][1], orderNumber, len(entrega)
         if orderNumber == row1[0][1] or len(entrega)< 6:
             products = {
                 " Blanca":9,
@@ -48,52 +49,54 @@ for row1 in itertools.izip_longest(csv_f1):
             
             try:
                 splited = row1[0][17].split('-')
-                print splited[1]
+                # print splited[1]
                 otherLine[index-1][products[splited[1]]]=subtot
             except:
                 try:
                     splited = row1[0][17].split(' ')
-                    print splited[0],row1[0][17].find('Medio kilo')
+                    # print splited[0],row1[0][17].find('Medio kilo')
                     if(row1[0][17].find('Medio kilo') != -1):
-                        otherLine[index-1][products[splited[0]]]="1/2 KG"
+                        otherLine[index-1][products[splited[0]]]="0.5"
                     else:
                         otherLine[index-1][products[splited[0]]]=subtot
                 except:
                     pass
-            print ">>>>>>>>",row1[0][17],"<<<<<<<<",otherLine[index-1]
+            # print ">>>>>>>>",row1[0][17],"<<<<<<<<",otherLine[index-1]
             # try:
             
             # except:
                 # print "This is an error message!"
 
         else:
-            print "No"
+            # print "No"
             countLine2 =0
             countLine3 =0
-            for row3 in itertools.izip_longest(csv_f3):
+            for row3 in itertools.zip_longest(csv_f3):
                 # print 'aaaaaaaaaaa',row3[0][4],row1[0][1]
                 if countLine2==0:
-                    print "Norrl"
+                    pass
                 else:
                     if row3[0][4]==row1[0][1]:
                         otherUserLine.append(['p',row3[0][0],row3[0][1],row3[0][2],row3[0][3],row3[0][4]+" ",row3[0][5],row1[0][3][:10],row1[0][11]])
-            countLine3 += 1
-            for row2 in itertools.izip_longest(csv_f2):
+                    else:
+                        pass
+                countLine3 += 1
+            for row2 in itertools.zip_longest(csv_f2):
                 if countLine2==0:
-                    print "Norrl"
+                    pass
                 else: 
                     if row2[0][2]==row1[0][1]:
                         entrega = row1[0][17].split()
-                        print "-->",entrega
+                        # print "-->",entrega
                         club = row2[0][17].split()
-                        print "a--->",club
+                        # print "a--->",club
                         club = ' '.join(map(str, club[1:]))
                         index += 1
                         otherLine.append([row1[0][0],row2[0][0]+' '+row2[0][1],row1[0][1],row2[0][12],row1[0][33],entrega[6],club,row1[0][3][:10],row1[0][11],'','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
                         # temp=([row1[0][0],row2[0][0]+' '+row2[0][1],row1[0][1],row2[0][12],row1[0][33],entrega[6],club,row1[0][3][:10],row1[0][11]],)
-                        print otherLine
+                        # print otherLine
                     else:
-                        print row1[0][0], row2[0][0]
+                        pass
                 countLine2 += 1
         orderNumber = row1[0][1]
     countLine += 1
@@ -101,8 +104,8 @@ for row1 in itertools.izip_longest(csv_f1):
     next(csv_f2)
     f3.seek(0) 
     next(csv_f3)
-print countLine
-with open('entrega.csv', 'wb') as writeFile:
+# print countLine
+with open('entrega.csv', 'w', newline='') as writeFile:
     writer = csv.writer(writeFile,delimiter=',')
     writer.writerows(lines) 
     writer.writerows(otherLine) 
@@ -116,7 +119,7 @@ with open('entrega.csv', mode='rt') as f, open('entrega_final.csv', 'w') as fina
     for row in sorted2: 
         writer.writerow(row)
 
-with open('entrega_usuarios.csv', 'wb') as writeFile:
+with open('entrega_usuarios.csv', 'w', newline='') as writeFile:
     writer = csv.writer(writeFile,delimiter=',')
     writer.writerows(userLines) 
     writer.writerows(otherUserLine) 
